5af47d27df01cb216e01df9b0605623923ebec4b
Natheer Radhi <natheer@extbh.dev>
2025-05-19 16:33:26 +0300

    Refactor render method in BaseController to accept generic types for view and data parameters

--- controllers/BaseController.php
+++ controllers/BaseController.php
@@ -4,7 +4,7 @@ abstract class BaseController
     // The index method is abstract and must be defined in any subclass
     // abstract public function index();
 
-    protected function render(string $view, array $data = []): void
+    protected function render($view, $data = []): void
     {
         // Extract data to make variables available in view
         extract($data);
