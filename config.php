60f9b777491221ff3935e24499a6c541502d9a13
Natheer Radhi <natheer@extbh.dev>
2025-05-19 16:31:11 +0300

    Update database configuration for server deployment

--- config.php
+++ config.php
@@ -6,12 +6,12 @@ error_reporting(E_ALL);
 
 
 // for local server use this to tunnel the db: `ssh -N -L 3306:localhost:3306 u202203102@108.142.248.238`
-// define('PREFIX', '/~u202203102/Project'); // for server
-define('PREFIX', ''); // for mac or local server
+define('PREFIX', '/~u202203102/Project'); // for server
+// define('PREFIX', ''); // for mac or local server
 
 
-define('DB_HOST', '127.0.0.1'); // for mac or local server
-// define('DB_HOST', 'localhost'); // for server
+// define('DB_HOST', '127.0.0.1'); // for mac or local server
+define('DB_HOST', 'localhost'); // for server
 
 
 define('DB_NAME', 'db202203102');
